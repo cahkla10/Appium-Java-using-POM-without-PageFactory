745300131e3ee382fef8938f4acd0dad8214e497
Cahkla10 <32485683+cahkla10@users.noreply.github.com>
2023-01-06 10:14:45 +0700

    Update ProductsPage.java

--- src/test/java/pages/ProductsPage.java
+++ src/test/java/pages/ProductsPage.java
@@ -1,18 +1,20 @@
 package pages;
 
 import helpers.Common;
+import io.appium.java_client.MobileBy;
+import org.openqa.selenium.By;
 
 public class ProductsPage {
 
-    private String productTitlePage = "//*[@text='PRODUCTS']";
-    private String productName = "test-Item title";
-    private String productPrice = "test-Price";
-    private String addToCartBtn = "test-ADD TO CART";
-    private String removeBtn = "test-REMOVE";
+    private By productTitlePage = By.xpath("//*[@text='PRODUCTS']");
+    private MobileBy productName = (MobileBy) MobileBy.AccessibilityId("test-Item title");
+    private MobileBy productPrice = (MobileBy) MobileBy.AccessibilityId("test-Price");
+    private MobileBy addToCartBtn = (MobileBy) MobileBy.AccessibilityId("test-ADD TO CART");
+    private MobileBy removeBtn = (MobileBy) MobileBy.AccessibilityId("test-REMOVE");
 
     Common common = new Common();
 
     public void readTitlePage(String exTitle) {
-        common.verifyXpathText(productTitlePage, exTitle);
+        common.verifyText(productTitlePage, exTitle);
     }
 }
